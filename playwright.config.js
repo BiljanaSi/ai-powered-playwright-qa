93ce98bf437b9f2e9b5c9e45ee1ab53cbfdf6ce4
BiljanaSi <bilja.test.sajt@gmail.com>
2026-08-19 14:27:16 +0200

    Configure Playwright HTML reporter

--- playwright.config.js
+++ playwright.config.js
@@ -3,6 +3,8 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
 
+  reporter: [['html', { open: 'never' }]],
+
   use: {
     baseURL: 'https://www.saucedemo.com',
     trace: 'on-first-retry',
